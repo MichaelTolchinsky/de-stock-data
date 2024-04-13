90cd3fb74febf5c439ddfa139f4e51a5e42b3bb9
Michael Tolchinsky <62743973+MichaelTolchinsky@users.noreply.github.com>
2024-04-13 19:36:33 +0300

    update imports (#5)

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,17 +1,16 @@
 from db import database as db
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 
 def process_data(raw_data):
-  symbol, date, close = raw_data
-  transformed_data = {'symbol': symbol, 'date': date, 'close': close}
-  return transformed_data
+    symbol, date, close = raw_data
+    transformed_data = {'symbol': symbol, 'date': date, 'close': close}
+    return transformed_data
+
 
 def process_and_save_data():
-  raw_data = db.fetch_raw_data()
-  processed_data = process_data(raw_data)
-  db.save_processed_data(processed_data)
+    raw_data = db.fetch_raw_data()
+    processed_data = process_data(raw_data)
+    db.save_processed_data(processed_data)
 
-if __name__ == "__main__":
-  process_and_save_data()
+# if __name__ == "__main__": only use for manual testing
+#   process_and_save_data()
