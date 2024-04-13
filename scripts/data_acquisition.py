35d5e32ce74feffa756802e115d4b3c5bee9cddb
Michael Tolchinsky <mishatolchinski@gmail.com>
2024-04-13 19:35:00 +0300

    update imports

--- scripts/data_acquisition.py
+++ scripts/data_acquisition.py
@@ -1,9 +1,7 @@
-from dotenv import load_dotenv
 from db import database as db
+from dotenv import load_dotenv
 import requests
 import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 load_dotenv()
 API_KEY = os.environ.get("ALPHA_VANTAGE_API_KEY")
@@ -13,22 +11,23 @@ SYMBOL = "NVDA"  # stock of your choise
 
 
 def fetch_stock_data():
-  params = {
-    "function": FUNCTION,
-    "symbol": SYMBOL,
-    "apikey": API_KEY,
-  }
+    params = {
+        "function": FUNCTION,
+        "symbol": SYMBOL,
+        "apikey": API_KEY,
+    }
 
-  response = requests.get(BASE_URL, params=params)
-  if response.status_code == 200:
-    return response.json()['Time Series (Daily)']
-  else:
-    print("Failed to fetch data", response.status_code)
+    response = requests.get(BASE_URL, params=params)
+    if response.status_code == 200:
+        return response.json()['Time Series (Daily)']
+    else:
+        print("Failed to fetch data", response.status_code)
+
 
 def fetch_and_save_raw_stock_data():
-  db.create_or_update_tables()
-  raw_data = fetch_stock_data()
-  db.save_raw_data(SYMBOL, raw_data)
+    db.create_or_update_tables()
+    raw_data = fetch_stock_data()
+    db.save_raw_data(SYMBOL, raw_data)
 
-if __name__ == "__main__":
-  fetch_and_save_raw_stock_data()
+# if __name__ == "__main__": only use for manual testing
+#   fetch_and_save_raw_stock_data()
