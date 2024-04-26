45cf2f7ae35df810c5941a6ebbf406e2657fae18
Michael Tolchinsky <62743973+MichaelTolchinsky@users.noreply.github.com>
2024-04-26 15:20:02 +0300

    add data models & functions input/output types (#10)

--- scripts/data_acquisition.py
+++ scripts/data_acquisition.py
@@ -4,6 +4,7 @@ import sys
 # Add the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from db import database as db
+from models.raw_stock_data import RawStockData
 
 API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
 BASE_URL = os.getenv("ALPHA_VANTAGE_BASE_URL")
@@ -11,7 +12,7 @@ FUNCTION = 'Time_Series_Daily'
 SYMBOL = "NVDA"  # stock of your choise
 
 
-def fetch_stock_data():
+def fetch_stock_data() -> None:
     params = {
         "function": FUNCTION,
         "symbol": SYMBOL,
@@ -20,12 +21,23 @@ def fetch_stock_data():
 
     response = requests.get(BASE_URL, params=params)
     if response.status_code == 200:
-        return response.json()['Time Series (Daily)']
+        data = response.json()['Time Series (Daily)']
+        # Extract data and create list of DailyStockData objects
+        raw_stock_data_list = []
+        for date, values in data:
+            raw_data = RawStockData(date=date,
+                                    open=float(values['open']),
+                                    high=float(values['high']),
+                                    low=float(values['float']),
+                                    close=float(values['close']),
+                                    volume=int(values['volume']))
+            raw_stock_data_list.append(raw_data)
+        return raw_stock_data_list
     else:
         print("Failed to fetch data", response.status_code)
 
 
-def fetch_and_save_raw_stock_data():
+def fetch_and_save_raw_stock_data() -> None:
     db.create_or_update_tables()
     raw_data = fetch_stock_data()
     db.save_raw_data(SYMBOL, raw_data)
--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -3,18 +3,20 @@ import sys
 # Add the parent directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from db import database as db
+from models.processed_stock_data import ProcessedStockData
+from models.raw_stock_data import RawStockData
 
 
-def process_data(raw_data):
-  symbol, date, close = raw_data
-  transformed_data = {'symbol': symbol, 'date': date, 'close': close}
-  return transformed_data
+def process_data(raw_data: RawStockData) -> ProcessedStockData:
+    symbol, date, close = raw_data
+    transformed_data = {'symbol': symbol, 'date': date, 'close': close}
+    return transformed_data
 
 
-def process_and_save_data():
-  raw_data = db.fetch_raw_data()
-  processed_data = process_data(raw_data)
-  db.save_processed_data(processed_data)
+def process_and_save_data() -> None:
+    raw_data = db.fetch_raw_data()
+    processed_data = process_data(raw_data)
+    db.save_processed_data(processed_data)
 
 # if __name__ == "__main__": only use for manual testing
 #   process_and_save_data()
